b1bd6265347e67f8d4f08a76a7cb43d42d69dbd9
William Tome <william.tomenh@gmail.com>
2023-02-14 16:41:05 -0300

    learning about strings.

---
+++ string.rb
@@ -0,0 +1,24 @@
+name = 'William'
+
+message = "Bem vindo #{name}"
+
+puts message
+
+message = 'Bem vindo #{name}'
+
+puts message
+
+message = <<MESSAGE
+
+  Bem vindo ao meu programa, #{name + '. Até logo!'}
+
+MESSAGE
+
+puts message
+
+message = %q[Olá João, tudo jóia?]
+
+puts message
+
+puts "O nome #{name} tem #{name.length} letras."
+
