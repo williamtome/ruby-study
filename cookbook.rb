dc986d69180281793f8e8cd619a61d01044a8ffd
William Tome <william.tomenh@gmail.com>
2023-02-20 12:47:09 -0300

    refctoring methods

--- cookbook.rb
+++ cookbook.rb
@@ -1,51 +1,55 @@
+puts "=================================================="
 puts "Bem-vindo ao Cookbook, sua rede social de receitas"
-
-$recipes = []
+puts "=================================================="
 
 def menu
+  puts
   puts "Escolha a opção desejada:"
   puts "[1] - Cadastrar uma receita" 
   puts "[2] - Mostrar as receitas" 
   puts "[3] - Sair"
   print "Opção desejada: "
-  return gets.to_i
+  gets.to_i
 end
 
-def register_recipe(recipe, type)
-  $recipes << { recipe: recipe, type: type }
+def register_recipe()
   puts
-  puts "Receita #{recipe} cadastrada com sucesso!"  
-  puts
+  puts "Digite o nome da receita"
+  recipe_name = gets.chomp
+  puts "Digite o tipo da receita"
+  type = gets.chomp
+  { recipe: recipe_name, type: type }
 end
 
-def show_recipes
+def show_recipes(r)
   puts
   puts "============== Receitas cadastradas =============="
-  $recipes.each do |recipe|
+  r.each do |recipe|
     puts "#{recipe[:recipe]} - #{recipe[:type]}"
   end
   puts
 end
 
-option = self.menu
+def show_invalid_option
+  puts
+  puts 'Opção inválida!'
+  puts
+end
+
+recipes = []
+
+option = menu
 
 while(option != 3)
   if option == 1
-    puts
-    puts "Digite o nome da receita"
-    recipe_name = gets.chomp
-    puts "Digite o tipo da receita"
-    type = gets.chomp
-    self.register_recipe(recipe_name, type)
-    option = self.menu
+    recipes << register_recipe
+    option = menu
   elsif(option == 2)  
-    self.show_recipes
-    option = self.menu
+    show_recipes(recipes)
+    option = menu
   else
-    puts
-    puts 'Opção inválida!'
-    puts
-    option = self.menu
+    show_invalid_option
+    option = menu
   end
 end
 
