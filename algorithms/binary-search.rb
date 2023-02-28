cc4c3b22f795a617733b7353c49ebb416b40e74c
William Tome <william.tomenh@gmail.com>
2023-02-27 23:00:39 -0300

    feat: finish to implement algorithm.

--- algorithms/binary-search.rb
+++ algorithms/binary-search.rb
@@ -1,5 +1,5 @@
-numbers = [10, 20, 30, 40, 50, 60, 70]
-number_search = 50
+numbers = [10, 20, 30, 50, 60, 80, 110, 130, 140, 170]
+number_search = 170
 low_index = 0
 high_index = numbers.length - 1
 
@@ -10,11 +10,13 @@ def binary_search(arr, number_search, low_index, high_index)
 
   return middle_index if number_search == arr[middle_index]
 
-  binary_search(arr, number_search, middle_index + 1, high_index) if number_search > arr[middle_index]
+  return binary_search(arr, number_search, middle_index + 1, high_index) if number_search > arr[middle_index]
 
-  binary_search(arr, number_search, low_index, middle_index - 1) if number_search < arr[middle_index]
+  return binary_search(arr, number_search, low_index, middle_index - 1) if number_search < arr[middle_index]
 end
 
 result = binary_search(numbers, number_search, low_index, high_index)
 
-puts result != -1 ? "Numero encontrado no índice #{result}" : "Numero não encontrado."
+puts result != -1 
+  ? "Numero encontrado no índice #{result}"
+  : "Numero não encontrado."
