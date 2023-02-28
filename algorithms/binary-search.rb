93e53bc3912e1eb450bc25e50f62b0c1aad15ee6
William Tome <william.tomenh@gmail.com>
2023-02-27 22:36:11 -0300

    feat: trying to implement algorithm

--- algorithms/binary-search.rb
+++ algorithms/binary-search.rb
@@ -1,24 +1,20 @@
 numbers = [10, 20, 30, 40, 50, 60, 70]
 number_search = 50
 low_index = 0
-high_index = arr.length - 1
+high_index = numbers.length - 1
 
 def binary_search(arr, number_search, low_index, high_index)
-  middle_index = high_index / 2
+  return -1 if low_index > high_index
+  
+  middle_index = (low_index + high_index) / 2
 
   return middle_index if number_search == arr[middle_index]
 
-  high_index = arr[middle_index - 1] if number_search < arr[middle_index]
+  binary_search(arr, number_search, middle_index + 1, high_index) if number_search > arr[middle_index]
 
-  low_index = arr[middle_index + 1] if number_search > arr[middle_index]
-  
-  arr[low_index..high_index].each_with_index do |number, index|
-    return index if number_search == number
-  end
-
-  return -1
+  binary_search(arr, number_search, low_index, middle_index - 1) if number_search < arr[middle_index]
 end
 
-result = binary_search(numbers, number_search)
+result = binary_search(numbers, number_search, low_index, high_index)
 
 puts result != -1 ? "Numero encontrado no índice #{result}" : "Numero não encontrado."
