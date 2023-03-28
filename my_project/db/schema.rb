c8e1d6a6d245c0f81c1d6dd694f3d7e3e6dc852e
William Tomé <william.tomenh@gmail.com>
2023-03-28 07:38:23 -0300

    fix(migration): add rule in name column

--- my_project/db/schema.rb
+++ my_project/db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_27_105633) do
   create_table "products", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.decimal "price"
     t.boolean "active"
     t.datetime "created_at", null: false
