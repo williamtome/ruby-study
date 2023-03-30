51ca063fa2404f649ffc77a746dd169026062cd8
William Tomé <william.tomenh@gmail.com>
2023-03-30 08:46:30 -0300

    feat(bubble_sort): trying to implement algorithm

--- algorithms/bubble-sort.rb
+++ algorithms/bubble-sort.rb
@@ -1,5 +1,15 @@
+# https://www.honeybadger.io/blog/ruby-bubble-sort/
 def bubble_sort(array)
-  for i in array.length - 1 do
-    
+  array_length = array.size
+
+  return array if array_length <= 1
+
+  loop do
+    swap = false
+    (array_length-1).times do |i|
+    end
   end
 end
+
+numbers = [5, 1, 4, 2, 8]
+puts bubble_sort(numbers)
